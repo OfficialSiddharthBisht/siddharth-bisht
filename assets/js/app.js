31bce637028e2abb161cee3a32b2fdba61be8cd8
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-01 19:42:04 +0530

    added resume to the site

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,6 +27,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Tech Stack</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1mOnwt-NA9tSJOPIUCh5idax5kaBZLSJV/view?usp=sharing" target="_blank">Resume</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://medium.com/@officialsiddharthbisht" target="_blank">Blogs</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
